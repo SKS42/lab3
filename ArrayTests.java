2ce99f1a3abaec8cd712376b9bc40d14ee9150af
SKS42 <sks001@ucsd.edu>
2023-01-26 15:31:02 -0800

    I changed ArrayExample and Array test

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,30 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1,2,3,4};
+    int[] expected = {1,2,3,4};
+    ArrayExamples.reversed(input1);
+    assertArrayEquals(expected, input1);
+  }
+
+  @Test
+  public void testReverseInPlace2(){
+    int[] input1 = {1,2,3,4};
+    int[] expected = {4,3,2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(expected, input1);
+  }
+
+
+  @Test
+  public void testReversed3(){
+    int[] expected = {4,3,2,1};
+    int[] input1 = {1,2,3,4};
+    assertArrayEquals(expected, ArrayExamples.reversed(input1));
+  }
 }
